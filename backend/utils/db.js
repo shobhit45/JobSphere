f83ccb425e08f588bc8c4397f14e65255179597d
shobhit45 <130762321+shobhit45@users.noreply.github.com>
2024-09-21 20:11:09 +0530

    changes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+      
+        
         await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully');
     } catch (error) {
