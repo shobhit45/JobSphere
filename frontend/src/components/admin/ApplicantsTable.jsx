da83bbd3c70f528aab8d7340911579cfac91a4b9
shobhit45 <130762321+shobhit45@users.noreply.github.com>
2025-04-02 22:48:47 +0530

    hi

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -52,7 +52,9 @@ const ApplicantsTable = () => {
                                         item.applicant?.profile?.resume ? <a className="text-blue-600 cursor-pointer" href={item?.applicant?.profile?.resume} target="_blank" rel="noopener noreferrer">{item?.applicant?.profile?.resumeOriginalName}</a> : <span>NA</span>
                                     }
                                 </TableCell>
-                                <TableCell>{item?.applicant.createdAt.split("T")[0]}</TableCell>
+                                <TableCell>
+                                    {item?.applicant?.createdAt ? item?.applicant.createdAt.split("T")[0] : "NA"}
+                                </TableCell>
                                 <TableCell className="float-right cursor-pointer">
                                     <Popover>
                                         <PopoverTrigger>
